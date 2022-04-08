e382081e49e9ce8dc5a1fed1de8d48a6b54d0f51
Aimad Benzakry <aimadbenzakry@gmail.com>
2022-04-08 21:38:09 +0000

    Initial commit

---
+++ src/main/java/com/example/gestion_agence_radeema/GestionAgenceRadeemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_agence_radeema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAgenceRadeemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAgenceRadeemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestion_agence_radeema/GestionAgenceRadeemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_agence_radeema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAgenceRadeemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
